d5f62cad5337cc296b319a84c7a6905089ea08ca
ChaeyeonHan <kate2236e@naver.com>
2022-07-03 15:46:17 +0900

    Initial commit

---
+++ src/main/java/com/its/mergeplan_v1/MergeplanV1Application.java
@@ -0,0 +1,13 @@
+package com.its.mergeplan_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeplanV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeplanV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/its/mergeplan_v1/MergeplanV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.mergeplan_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeplanV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
