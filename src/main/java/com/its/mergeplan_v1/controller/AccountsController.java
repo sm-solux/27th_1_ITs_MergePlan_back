f82ed4de3b2743b6fff0f559ccf0d96136123779
= <koeunseo@sookmyung.ac.kr>
2022-08-07 16:02:40 +0900

    [FEAT] Accounts Item - patch, delete

--- src/main/java/com/its/mergeplan_v1/controller/AccountsController.java
+++ src/main/java/com/its/mergeplan_v1/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.its.mergeplan_v1.controller;
 
 import com.its.mergeplan_v1.config.auth.PrincipalDetails;
+import com.its.mergeplan_v1.dto.PatchAccountsItem;
 import com.its.mergeplan_v1.dto.PostAccountsCategory;
 import com.its.mergeplan_v1.dto.PostAccountsItem;
 import com.its.mergeplan_v1.entity.AccountsCategory;
@@ -24,17 +25,32 @@ public class AccountsController {
         return accountsService.saveCategory(principalDetails, postAccountsCategory);
     }
 
+    @GetMapping("/auth/accounts/category")
+    public List<AccountsCategory> getCategory(){
+        return accountsService.getCategory();
+    }
+
     @PostMapping("/auth/accounts/item")
     public AccountsItem setItem(Authentication auth, @RequestBody PostAccountsItem postAccountsItem){
         PrincipalDetails principalDetails = (PrincipalDetails) auth.getPrincipal();
         return accountsService.saveItem(principalDetails, postAccountsItem);
     }
 
-    // 쓸 일 없는 확인용 값
     @GetMapping("/auth/accounts/item")
     public List<AccountsItem> getItem(Authentication auth){
         PrincipalDetails principalDetails = (PrincipalDetails) auth.getPrincipal();
         return accountsService.getItem(principalDetails);
+    }
+
+    @PatchMapping("/auth/accounts/item")
+    public AccountsItem patchItem(@RequestBody PatchAccountsItem patchAccountsItem){
+        return accountsService.saveItem(patchAccountsItem);
+    }
+
+    @DeleteMapping("/auth/accounts/item/{id}")
+    public String deleteItem(@PathVariable("id") int id){
+        accountsService.deleteItem(id);
+        return "test!";
     }
 
     @GetMapping("/auth/accounts/item/{year}/{month}")
