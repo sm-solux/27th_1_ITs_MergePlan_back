80faec180c16d05b2e6cc79422b9416ee3bc92c4
ChaeyeonHan <kate2236e@naver.com>
2022-08-08 09:40:11 +0900

    [FEAT] Planner page

--- src/main/java/com/its/mergeplan_v1/service/PlannerService.java
+++ src/main/java/com/its/mergeplan_v1/service/PlannerService.java
@@ -8,8 +8,10 @@ import com.its.mergeplan_v1.repository.PlannerRepository;
 import com.its.mergeplan_v1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -19,9 +21,53 @@ public class PlannerService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Planner createPlan(PrincipalDetails principalDetails, PostPlanner postPlannerReq){
-        User user = userRepository.findById(principalDetails.getUser().getId());
+    public Planner setPlan(User loginUser, PostPlanner postPlannerReq){
+        User user = userRepository.findById(loginUser.getId());
         Planner planner = postPlannerReq.toEntity(postPlannerReq.isAllday(), postPlannerReq.getStartTime(), postPlannerReq.getEndTime(), postPlannerReq.getTitle(), postPlannerReq.getCategory(), postPlannerReq.getDescription());
         return plannerRepository.save(planner);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostPlanner> mainPlannerPage(User loginUser, Long user_id){
+        User user = userRepository.findById(loginUser.getId());
+        List<Planner> plannerList = plannerRepository.findByUserId(user_id);
+        List<PostPlanner> postPlannerLists = new ArrayList<>();
+
+        for (Planner pn: plannerList){
+            PostPlanner postPlanner = new PostPlanner();
+            postPlanner.setTitle(pn.getTitle());
+            postPlanner.setAllday(pn.isAllday());
+            postPlanner.setCategory(pn.getCategory());
+            postPlanner.setStartTime(pn.getStartDatetime());
+            postPlanner.setEndTime(pn.getEndDatetime());
+            postPlanner.setDescription(pn.getDescription());
+            postPlanner.setUser(pn.getUser());
+
+            postPlannerLists.add(postPlanner);
+        }
+
+        return postPlannerLists;
+    }
+
+    @Transactional
+    public Planner updatePlan(User user, PostPlanner postPlanner, int plan_id){
+        Planner origin_planner = plannerRepository.findById(plan_id);
+        if (origin_planner.getUser().getId() == user.getId()){
+            origin_planner.setDescription(postPlanner.getDescription());
+            origin_planner.setCategory(postPlanner.getCategory());
+            origin_planner.setStartDatetime(postPlanner.getStartTime());
+            origin_planner.setEndDatetime(postPlanner.getEndTime());
+            origin_planner.setAllday(postPlanner.isAllday());
+
+            return origin_planner;
+        } else{
+            return null;
+        }
+    }
+
+    @Transactional
+    public void deletePlan(User loginUser, int plan_id){
+        Planner planner = plannerRepository.findById(plan_id);
+        plannerRepository.delete(planner);
     }
 }
