b658e8d74c90a12bca140b779703a3af4a1a9ce4
ChaeyeonHan <kate2236e@naver.com>
2022-08-12 23:43:09 +0900

    [MOD] planner service

---
+++ src/main/java/com/its/mergeplan_v1/dto/GetPlanner.java
@@ -0,0 +1,44 @@
+package com.its.mergeplan_v1.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.its.mergeplan_v1.entity.Planner;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPlanner {
+    private Long userId;
+    private boolean allDay;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date start;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date end;
+    private String title;
+    private int category;
+    private String description;
+    private String itemFirstWord;
+
+
+    public Planner toEntity(Long userId, boolean allDay, Date start, Date end, String title, int category, String description){
+        String[] str = {"Work", "Party", "Shopping", "Dining", "Trip"};
+        return Planner.builder()
+                .userId(userId)
+                .allday(allDay)
+                .startDatetime(start)
+                .endDatetime(end)
+                .title(title)
+                .itemFirst(category)
+                .itemFirstWord(str[category - 1])
+                .description(description).build();
+    }
+
+
+}
